d1fd2dffe1903308461be48e242e55c019badf32
DONALDBZR <andygaspard@hotmail.com>
2023-06-10 10:12:46 +0400

    [UPDATE 0.0.6] Adding the routing of the application

--- Routes.php
+++ Routes.php
@@ -2,5 +2,7 @@
 require_once "{$_SERVER['DOCUMENT_ROOT']}/Models/Router.php";
 $Router = new Router($_SERVER['REQUEST_URI']);
 switch ($Router->getRoute()) {
-    
+    case '/':
+        $Router->get($Router->getRoute(), "/Views/Homepage.php");
+        break;
 }
