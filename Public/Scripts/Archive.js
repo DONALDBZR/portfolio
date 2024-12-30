b3f5c00725ed09b2c73963086a7770c051901c37
DONALDBZR <andygaspard@hotmail.com>
2024-12-30 19:26:33 +0400

    [UPDATE 2.2.2] The Application that is going to be rendered in the DOM

--- Public/Scripts/Archive.js
+++ Public/Scripts/Archive.js
@@ -32,7 +32,7 @@ class Application extends React.Component {
                     status: "Complete",
                 },
                 {
-                    year: 2023,
+                    year: 2024,
                     name: "FinID",
                     from: "Finance Club LTD",
                     technologiesUsed: [
